9f569a7431e4cebab648b9dcec6f80a577d2c3c5
supertom495 <supertom495@gmail.com>
2020-06-19 14:06:08 +1000

    cirtificate logic

--- script.go
+++ script.go
@@ -35,50 +35,118 @@ func main() {
 	fmt.Println(certificatePath) // for example /home/user
 
 	// creating certificate files using cmd
-	app := "mkdir"
+	generateCertificate()
 
-	arg0 := "myfile"
+}
 
-	cmd := exec.Command(app, arg0)
+func generateCertificate() {
+	genRootCA()
+	reqRootCA()
+	genDevice()
+	reqDevice()
+	reqCrt()
+}
+
+func genRootCA() {
+	// 1
+	app := "openssl"
+	arg0 := "genrsa"
+	arg1 := "-out"
+	arg2 := "rootCA.key"
+	arg3 := "2048"
+	cmd := exec.Command(app, arg0, arg1, arg2, arg3)
 	stdout, err := cmd.Output()
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-
 	fmt.Print(string(stdout))
-
 }
 
-func sub() {
-
-	path, err := os.Getwd()
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println(path) // for example /home/user
-
-	os.Chdir("./certificate")
-	a, err := os.Getwd()
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println(a) // for example /home/user
-
-	app := "echo"
-
-	// arg0 := "-e"
-	arg1 := "Hello world"
-	// arg2 := "\n\tfrom"
-	// arg3 := "golang"
-
-	cmd := exec.Command(app, arg1)
+func genDevice() {
+	// 3
+	app := "openssl"
+	arg0 := "genrsa"
+	arg1 := "-out"
+	arg2 := "device.key"
+	arg3 := "2048"
+	cmd := exec.Command(app, arg0, arg1, arg2, arg3)
 	stdout, err := cmd.Output()
-
 	if err != nil {
-		fmt.Println(err.Error())
+		log.Fatal(err)
 		return
 	}
+	fmt.Print(string(stdout))
+}
 
+func reqRootCA() {
+	// 2
+	app := "openssl"
+	arg0 := "req"
+	arg1 := "-x509"
+	arg2 := "-new"
+	arg3 := "-nodes"
+	arg4 := "-key"
+	arg5 := "rootCA.key"
+	arg6 := "-subj"
+	arg7 := "/CN=" + NGROK_DOMAIN
+	arg8 := "-days"
+	arg9 := "5000 "
+	arg10 := "-out"
+	arg11 := "rootCA.pem"
+
+	cmd := exec.Command(app, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11)
+	stdout, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Print(string(stdout))
+}
+
+func reqDevice() {
+	// 4
+	app := "openssl"
+	arg0 := "req"
+	arg1 := "-new"
+	arg2 := "-key"
+	arg3 := "device.key"
+	arg4 := "-subj"
+	arg5 := "/CN=" + NGROK_DOMAIN
+	arg6 := "-out"
+	arg7 := "device.csr"
+
+	cmd := exec.Command(app, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7)
+	stdout, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Print(string(stdout))
+}
+
+func reqCrt() {
+	// 5
+	app := "openssl"
+	arg0 := "x509"
+	arg1 := "-req"
+	arg2 := "-in"
+	arg3 := "device.csr"
+	arg4 := "-CA"
+	arg5 := "rootCA.pem"
+	arg6 := "-CAkey"
+	arg7 := "rootCA.key"
+	arg8 := "-CAcreateserial"
+	arg9 := "-out"
+	arg10 := "device.crt"
+	arg11 := "-days"
+	arg12 := "5000"
+
+	cmd := exec.Command(app, arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12)
+	stdout, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 	fmt.Print(string(stdout))
 }
